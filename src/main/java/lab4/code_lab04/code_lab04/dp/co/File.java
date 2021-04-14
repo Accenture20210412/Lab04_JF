903ec556784963b87521bbe9cbc5aff5ab126ea0
Jakub Fik <fikkuba@gmail.com>
2021-04-14 09:25:17 +0200

    init

---
+++ src/main/java/lab4/code_lab04/code_lab04/dp/co/File.java
@@ -0,0 +1,36 @@
+package lab4.code_lab04.code_lab04.dp.co;
+
+public class File implements FileSystemElement {
+
+	private String name;
+	
+	public File(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public boolean rename(String newName) {
+		this.name = newName;
+		return true;
+	}
+
+	@Override
+	public boolean remove() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean move() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void print() {
+		System.out.println(name);
+	}
+	
+	
+
+}
---
+++ src/main/java/lab4/code_lab04/code_lab04/dp/co/FileSystemElement.java
@@ -0,0 +1,10 @@
+package lab4.code_lab04.code_lab04.dp.co;
+
+public interface FileSystemElement {
+
+	public boolean rename(String newName);
+	public boolean remove();
+	public boolean move();
+	public void print();
+	
+}
